2ef4af9b257707fbe6a109e5b878d163d1a8f9fb
sun <sun>
2024-01-23 21:56:04 +0800

    add RWMutex

--- apps/gamex/internal/core/sceneManager/scene.go
+++ apps/gamex/internal/core/sceneManager/scene.go
@@ -1,6 +1,7 @@
 package sceneManager
 
 import (
+	"sync"
 	"zinx-zero/apps/gamex/internal/ice"
 
 	"github.com/spf13/cast"
@@ -10,22 +11,44 @@ import (
 var _ ice.IScene = (*Scene)(nil)
 
 type Scene struct {
+	sync.RWMutex
+
 	sceneId    int64
 	sceneIdStr string
 }
 
 // GetSceneId implements ice.IScene.
 func (a *Scene) GetSceneId() (sceneId int64) {
-	return a.sceneId
+	a.doRead(func() {
+		sceneId = a.sceneId
+	})
+	return sceneId
 }
 
 // GetSceneIdStr implements ice.IScene.
 func (a *Scene) GetSceneIdStr() (sceneIdStr string) {
-	return a.sceneIdStr
+	a.doRead(func() {
+		sceneIdStr = a.sceneIdStr
+	})
+	return sceneIdStr
 }
 
 // SetSceneId implements ice.IScene.
 func (a *Scene) SetSceneId(sceneId int64) {
-	a.sceneId = sceneId
-	a.sceneIdStr = cast.ToString(sceneId)
+	a.doWrite(func() {
+		a.sceneId = sceneId
+		a.sceneIdStr = cast.ToString(sceneId)
+	})
+}
+
+func (a *Scene) doWrite(fn func()) {
+	a.Lock()
+	defer a.Unlock()
+	fn()
+}
+
+func (a *Scene) doRead(fn func()) {
+	a.RLock()
+	defer a.RUnlock()
+	fn()
 }
